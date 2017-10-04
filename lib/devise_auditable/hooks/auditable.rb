9523de32789dc8d8ce588e558d8ef1606cbbddfc
Mike Stewart <mike-stewart@users.noreply.github.com>
2017-10-04 16:05:31 -0300

    Add skip logic.

--- lib/devise_auditable/hooks/auditable.rb
+++ lib/devise_auditable/hooks/auditable.rb
@@ -1,11 +1,11 @@
 Warden::Manager.after_authentication do |record, warden, opts|
-  if record.respond_to?(:audit_login!)
+  if record.respond_to?(:audit_login!) && !warden.request.env['devise.skip_auditable']
     record.audit_login!(warden.request)
   end
 end
 
 Warden::Manager.before_logout do |record, warden, opts|
-  if record.respond_to?(:audit_logout!)
+  if record.respond_to?(:audit_logout!) && !warden.request.env['devise.skip_auditable']
     record.audit_logout!(warden.request)
   end
 end
