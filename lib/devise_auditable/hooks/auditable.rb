f5c435c31228f02110c137fa7a8734c97beb5168
Mike Stewart <mike-stewart@users.noreply.github.com>
2017-10-20 18:16:14 -0300

    Keep the skip_trackable check, as it's used by timeoutable.

--- lib/devise_auditable/hooks/auditable.rb
+++ lib/devise_auditable/hooks/auditable.rb
@@ -1,11 +1,11 @@
 Warden::Manager.after_set_user :except => :fetch do |record, warden, opts|
-  if record.respond_to?(:audit_login!) && warden.authenticated?(opts[:scope]) && !warden.request.env['devise.skip_auditable']
+  if record.respond_to?(:audit_login!) && warden.authenticated?(opts[:scope]) && !warden.request.env['devise.skip_trackable']
     record.audit_login!(warden.request)
   end
 end
 
 Warden::Manager.before_logout do |record, warden, opts|
-  if record.respond_to?(:audit_logout!) && !warden.request.env['devise.skip_auditable']
+  if record.respond_to?(:audit_logout!) && !warden.request.env['devise.skip_trackable']
     record.audit_logout!(warden.request)
   end
 end
